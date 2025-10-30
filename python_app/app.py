4a31b089801292c914d13f7af09b396368247bc7
unknown <putta.sharath478@gmail.com>
2025-10-30 04:54:15 +0000

    Updated code

--- python_app/app.py
+++ python_app/app.py
@@ -1,18 +1,3 @@
-# # app.py
-# import os
-
-# def connect_to_service():
-#     # ❌ Hardcoded API key — will be caught by GitLeaks (generic-api-key / AWS / Token patterns)
-#     API_KEY = "ghp_abCDeFgHijKLMNOPqrstuVWxyZ012345678"
-    
-#     print("Connecting to external services with the provided API key...")
-#     print(f"Using API_KEY: {API_KEY[:6]}********")
-
-#     return True
-
-# if __name__ == "__main__":
-#     connect_to_service()
-
 
 import os
 from flask import Flask, jsonify
